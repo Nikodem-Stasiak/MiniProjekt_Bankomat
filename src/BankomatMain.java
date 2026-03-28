3924c04396a8a61de73354b91cfcd57131a2cf6f
Nikodem Sebastian Stasiak <194699854+Nikodem-Stasiak@users.noreply.github.com>
2026-03-28 21:37:10 +0100

    Dodanie pętli głównej bankomatu, menu użytkownika i możliwości otwarcia pustego konta

--- src/BankomatMain.java
+++ src/BankomatMain.java
@@ -1,14 +1,55 @@
+import java.util.Scanner;
+
 public class BankomatMain {
     public static void main(String[] args) {
-        KontoBankowe k1 = new KontoBankowe(1000);
-        k1.wyplac(500);
+        KontoBankowe k1 = new KontoBankowe();
+        Scanner skaner=new Scanner(System.in);
+        boolean czyDziala = true;
 
-        try{
-            k1.wyplac(2000);
-        } catch (Exception e) {
-            System.out.println("Transakcja odrzucona!");
+        while(czyDziala){
+            System.out.println("--- Menu Bankomatu ---");
+            System.out.println("1. Wypłata");
+            System.out.println("2. Wpłata");
+            System.out.println("3. Sprawdź stan konta");
+            System.out.println("4. Koniec");
+            System.out.println("Wybierz opcję: ");
+            int wybor = skaner.nextInt();
+
+            switch (wybor){
+                case 1:
+                {
+                    System.out.println("Podaj kwotę do wypłaty: ");
+                    int kwota = skaner.nextInt();
+                    try{
+                        k1.wyplac(kwota);
+                    }catch (Exception e){
+                        System.out.println("Transakcja odrzucona!");
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    System.out.println("Podaj kwotę do wpłaty: ");
+                    int kwota = skaner.nextInt();
+                    k1.wplac(kwota);
+                    break;
+                }
+                case 3:
+                {
+                    System.out.println("Aktualny stan konta wynosi: " + k1.getSaldo());
+                    break;
+                }
+                case 4:
+                {
+                    System.out.println("Dziękujemy za skorzystanie z bankomatu!");
+                    czyDziala = false;
+                    break;
+                }
+                default:
+                {
+                    System.out.println("Nieznana opcja!");
+                }
+            }
         }
-
-        System.out.println("Aktualny stan konta wynosi: " + k1.getSaldo());
     }
 }
--- src/KontoBankowe.java
+++ src/KontoBankowe.java
@@ -5,13 +5,17 @@ public class KontoBankowe {
         this.saldo = saldoPodane;
     }
 
+    public KontoBankowe() {
+        this.saldo = 0;
+    }
+
     public  int getSaldo(){
         return this.saldo;
     }
 
     public void wplac(int kwota){
         this.saldo += kwota;
-        System.out.println("Wpłacono: " + kwota + ". Stan konta obecnie wynosi: " + this.saldo);
+        System.out.println("Wpłacono: " + kwota);
     }
 
     public void wyplac(int kwota){
@@ -19,7 +23,7 @@ public class KontoBankowe {
             throw new IllegalArgumentException("Bład! Kwota którą chcesz wypłacić przekracza saldo na twoim koncie!");
         } else {
             this.saldo -= kwota;
-            System.out.println("Wypłacono: " + kwota + ". Stan konta obecnie wynosi: " + this.saldo);
+            System.out.println("Wypłacono: " + kwota);
         }
     }
 }

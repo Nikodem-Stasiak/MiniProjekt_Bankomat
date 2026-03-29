af41800335f1b0eabcbf34728073d19255094612
Nikodem Sebastian Stasiak <194699854+Nikodem-Stasiak@users.noreply.github.com>
2026-03-29 01:59:00 +0100

    V2.2 - Implementacja unikalności numerów kont i pełna walidacja danych

--- src/BankomatMain.java
+++ src/BankomatMain.java
@@ -6,101 +6,154 @@ import java.io.File;
 public class BankomatMain {
     public static void main(String[] args) {
         ArrayList<KontoBankowe> listaKont = new ArrayList<>();
-        Scanner skaner=new Scanner(System.in);
+        Scanner skaner = new Scanner(System.in);
         boolean czyDziala = true;
 
         File plikBazy = new File("baza_kont.txt");
 
-        if (plikBazy.exists()){
-            try(Scanner czytnikPliku = new Scanner(plikBazy)){
-                while (czytnikPliku.hasNextLine()){
+        if (plikBazy.exists()) {
+            try (Scanner czytnikPliku = new Scanner(plikBazy)) {
+                while (czytnikPliku.hasNextLine()) {
                     String linia = czytnikPliku.nextLine();
                     String[] czesci = linia.split(";");
                     String nr = czesci[0];
                     int p = Integer.parseInt(czesci[1]);
                     int s = Integer.parseInt(czesci[2]);
-                    listaKont.add(new KontoBankowe(nr,p,s));
+                    listaKont.add(new KontoBankowe(nr, p, s));
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Błąd podczas wczytywania bazy: " + e.getMessage());
             }
-        }else{
+        } else {
             listaKont.add(new KontoBankowe("12345", 1111, 5000));
             listaKont.add(new KontoBankowe("98765", 2222));
         }
 
-        KontoBankowe zalogowaneKonto = null;
+        while (czyDziala) {
+            System.out.println("--- Menu Główne ---");
+            System.out.println("1. Zaloguj się na istniejące konto.");
+            System.out.println("2. Załóż nowe konto.");
+            System.out.println("3. Koniec");
+            int wyborStart = skaner.nextInt();
+            switch (wyborStart) {
+                case 1: {
+                    KontoBankowe zalogowaneKonto = null;
 
-        System.out.println("Podaj numer konta: ");
-        String wpisanyNumer = skaner.next();
+                    System.out.println("Podaj numer konta: ");
+                    String wpisanyNumer = skaner.next();
 
-        System.out.println("Podaj PIN: ");
-        int wpisanyPin = skaner.nextInt();
+                    System.out.println("Podaj PIN: ");
+                    int wpisanyPin = skaner.nextInt();
 
-        for(KontoBankowe kontoZListy : listaKont){
-            if(wpisanyNumer.equals(kontoZListy.getNumerKonta())&& wpisanyPin == kontoZListy.getPin()){
-                zalogowaneKonto = kontoZListy;
-                break;
-            }
-        }
-
-        if(zalogowaneKonto != null){
-            System.out.println("Logowanie udane!");
-
-            while(czyDziala){
-                System.out.println("--- Menu Bankomatu ---");
-                System.out.println("1. Wypłata");
-                System.out.println("2. Wpłata");
-                System.out.println("3. Sprawdź stan konta");
-                System.out.println("4. Koniec");
-                System.out.println("Wybierz opcję: ");
-                int wybor = skaner.nextInt();
-
-                switch (wybor){
-                    case 1:
-                    {
-                        System.out.println("Podaj kwotę do wypłaty: ");
-                        int kwota = skaner.nextInt();
-                        try{
-                            zalogowaneKonto.wyplac(kwota);
-                        }catch (Exception e){
-                            System.out.println("Transakcja odrzucona!");
+                    for (KontoBankowe kontoZListy : listaKont) {
+                        if (wpisanyNumer.equals(kontoZListy.getNumerKonta()) && wpisanyPin == kontoZListy.getPin()) {
+                            zalogowaneKonto = kontoZListy;
+                            break;
                         }
-                        break;
                     }
-                    case 2:
-                    {
-                        System.out.println("Podaj kwotę do wpłaty: ");
-                        int kwota = skaner.nextInt();
-                        zalogowaneKonto.wplac(kwota);
-                        break;
+
+                    if (zalogowaneKonto != null) {
+                        System.out.println("Logowanie udane!");
+
+                        boolean sesjaAktywna = true;
+                        while (sesjaAktywna) {
+                            System.out.println("--- Menu Bankomatu ---");
+                            System.out.println("1. Wypłata");
+                            System.out.println("2. Wpłata");
+                            System.out.println("3. Sprawdź stan konta");
+                            System.out.println("4. Koniec");
+                            System.out.println("Wybierz opcję: ");
+                            int wybor = skaner.nextInt();
+
+                            switch (wybor) {
+                                case 1: {
+                                    System.out.println("Podaj kwotę do wypłaty: ");
+                                    int kwota = skaner.nextInt();
+                                    try {
+                                        zalogowaneKonto.wyplac(kwota);
+                                    } catch (Exception e) {
+                                        System.out.println("Transakcja odrzucona!");
+                                    }
+                                    break;
+                                }
+                                case 2: {
+                                    System.out.println("Podaj kwotę do wpłaty: ");
+                                    int kwota = skaner.nextInt();
+                                    zalogowaneKonto.wplac(kwota);
+                                    break;
+                                }
+                                case 3: {
+                                    System.out.println("Aktualny stan konta wynosi: " + zalogowaneKonto.getSaldo());
+                                    break;
+                                }
+                                case 4: {
+                                    System.out.println("Wylogowano.");
+                                    sesjaAktywna = false;
+                                    break;
+                                }
+                                default: {
+                                    System.out.println("Nieznana opcja!");
+                                }
+                            }
+                        }
+                    } else {
+                        System.out.println("Błędny numer konta lub PIN! Dostęp odmówiony!");
                     }
-                    case 3:
-                    {
-                        System.out.println("Aktualny stan konta wynosi: " + zalogowaneKonto.getSaldo());
-                        break;
+                    break;
+                }
+                case 2: {
+                    String nr = "";
+                    boolean numerJestOk = false;
+
+                    while(!numerJestOk){
+                        System.out.println("Stwórz numer konta (5 znaków): ");
+                        nr = skaner.next();
+                        if(nr.length() != 5){
+                            System.out.println("BŁĄD! Numer musi zawierać 5 znaków.");
+                            continue;
+                        }
+                        boolean znalezionoDuplikat = false;
+                        for(KontoBankowe k : listaKont){
+                            if(k.getNumerKonta().equals(nr)){
+                                znalezionoDuplikat=true;
+                                break;
+                            }
+                        }
+                        if(znalezionoDuplikat){
+                            System.out.println("BŁĄD! Konto o tym numerze już istnieje w naszym banku!");
+                        }else{
+                            numerJestOk = true;
+                        }
                     }
-                    case 4:
-                    {
-                        System.out.println("Dziękujemy za skorzystanie z bankomatu!");
-                        czyDziala = false;
-                        break;
+
+                    System.out.println("Stwórz PIN do konta (4 znaki): ");
+                    int p = skaner.nextInt();
+                    while(p < 1000 || p > 9999){
+                        System.out.println("BŁĄD! PIN musi zawierać 4 znaki. Podaj ponownie: ");
+                        p = skaner.nextInt();
                     }
-                    default:
-                    {
-                        System.out.println("Nieznana opcja!");
-                    }
+
+                    listaKont.add(new KontoBankowe(nr,p));
+                    System.out.println("Konto założone pomyślnie!");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Zamykanie systemu...");
+                    czyDziala = false;
+                    break;
+                }
+                default: {
+                    System.out.println("Nieznana opcja!");
                 }
             }
-        } else {
-            System.out.println("Błędny numer konta lub PIN! Dostęp odmówiony!");
         }
-        try (PrintWriter pisarz = new PrintWriter("baza_kont.txt")){
-            for (KontoBankowe k: listaKont){
+
+        try (PrintWriter pisarz = new PrintWriter("baza_kont.txt")) {
+            for (KontoBankowe k : listaKont) {
                 pisarz.println(k.getNumerKonta() + ";" + k.getPin() + ";" + k.getSaldo());
             }
             System.out.println("Dane zostały pomyślnie zapisane do bazy.");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Błąd zapisu do pliku: " + e.getMessage());
         }
     }

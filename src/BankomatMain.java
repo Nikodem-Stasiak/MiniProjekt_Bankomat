5dcda04efbe21838e4fa3a37f042e7ada60f3ae0
Nikodem Sebastian Stasiak <194699854+Nikodem-Stasiak@users.noreply.github.com>
2026-03-29 01:12:12 +0100

    Implementacja trwałego zapisu i odczytu danych z pliku (File I/O)

--- src/BankomatMain.java
+++ src/BankomatMain.java
@@ -1,5 +1,7 @@
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
 
 public class BankomatMain {
     public static void main(String[] args) {
@@ -7,8 +9,25 @@ public class BankomatMain {
         Scanner skaner=new Scanner(System.in);
         boolean czyDziala = true;
 
-        listaKont.add(new KontoBankowe("12345", 1111, 5000));
-        listaKont.add(new KontoBankowe("98765", 2222));
+        File plikBazy = new File("baza_kont.txt");
+
+        if (plikBazy.exists()){
+            try(Scanner czytnikPliku = new Scanner(plikBazy)){
+                while (czytnikPliku.hasNextLine()){
+                    String linia = czytnikPliku.nextLine();
+                    String[] czesci = linia.split(";");
+                    String nr = czesci[0];
+                    int p = Integer.parseInt(czesci[1]);
+                    int s = Integer.parseInt(czesci[2]);
+                    listaKont.add(new KontoBankowe(nr,p,s));
+                }
+            }catch (Exception e){
+                System.out.println("Błąd podczas wczytywania bazy: " + e.getMessage());
+            }
+        }else{
+            listaKont.add(new KontoBankowe("12345", 1111, 5000));
+            listaKont.add(new KontoBankowe("98765", 2222));
+        }
 
         KontoBankowe zalogowaneKonto = null;
 
@@ -75,6 +94,14 @@ public class BankomatMain {
             }
         } else {
             System.out.println("Błędny numer konta lub PIN! Dostęp odmówiony!");
+        }
+        try (PrintWriter pisarz = new PrintWriter("baza_kont.txt")){
+            for (KontoBankowe k: listaKont){
+                pisarz.println(k.getNumerKonta() + ";" + k.getPin() + ";" + k.getSaldo());
+            }
+            System.out.println("Dane zostały pomyślnie zapisane do bazy.");
+        } catch (Exception e){
+            System.out.println("Błąd zapisu do pliku: " + e.getMessage());
         }
     }
 }
